2c52246f4e058f4b5c939e44df0d20dfa2368c95
Uladzislau Pauliushchyk <u.pauliushchyk@codex-soft.com>
2020-09-24 14:13:14 +0300

    add resize logic to table, performance and UX adjustments

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,61 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
+  onMousedown(e) {
+    const $resizer = $(e.target);
+
+    const resizeType = $resizer.data.resize;
+    const isCol = resizeType === 'col';
+
+    if (resizeType) {
+      let value;
+      const initial = isCol ? 'width' : 'height';
+      const edge = isCol ? 'right' : 'bottom';
+      const mouseDimension = isCol ? 'pageX' : 'pageY';
+      const $resizable = $resizer.closest('[data-type="resizable"]');
+      const coords = $resizable.getCoords();
+      $resizer.css({opacity: 1});
+      document.body.style.cursor = isCol ? 'col-resize' : 'row-resize';
+
+      document.onmousemove = e => {
+        const delta = e[mouseDimension] - coords[edge];
+        $resizer.css({
+          [edge]: (- delta - 4) + 'px',
+          zIndex: 10,
+        })
+        value = coords[initial] + delta + 4;
+      }
+
+      document.onmouseup = () => {
+        if (isCol) {
+          this.$root.findAll(`[data-col="${$resizable.data.col}"]`)
+              .forEach(col => col.style[initial] = value + 'px');
+        } else {
+          $resizable.css({[initial]: value + 'px'})
+        }
+
+        $resizer.css({
+          opacity: '',
+          [edge]: '',
+          zIndex: '',
+        })
+
+        document.body.style.cursor = '';
+        document.onmousemove = null;
+
+        document.onmouseup = null;
+      }
+    }
+  }
 
   toHTML() {
     return createTable(40);
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,21 @@
 export class Dom {
   constructor(selector) {
     this.$el = typeof selector == 'string'
-      ? document.querySelector(selector)
-      : this.$el = selector;
+        ? document.querySelector(selector)
+        : this.$el = selector;
+  }
+
+  closest(selector) {
+    const el = this.$el.closest(selector)
+    return el ? $(el) : null;
+  }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
   }
 
   html(template) {
@@ -11,6 +24,11 @@ export class Dom {
       return this;
     }
     return this.$el.outerHTML.trim();
+  }
+
+  css(styles = {}) {
+    Object.assign(this.$el.style, styles)
+    return this;
   }
 
   on(eventType, callback) {
@@ -31,6 +49,16 @@ export class Dom {
   append(node) {
     this.$el.append(node instanceof Dom ? node.$el : node);
     return this;
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  find(selector) {
+    const el = this.$el.querySelector(selector);
+
+    return el ? $(el) : null;
   }
 }
 
